a82bdb36ee565bfd733c63a490f5a11fcd01b9e8
chenronggui <chenronggui@gomo.com>
2018-02-27 18:28:20 +0800

    完成项目的搭建

---
+++ app.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const router = require('koa-router')()
+
+const app = new Koa()
+const githubRouter = require('./router/github')
+const middleware = require('./middleware')
+middleware(app)
+// 考虑自动加载路由
+githubRouter(app)
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
+})
